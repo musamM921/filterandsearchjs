107898e1941b19c658cf0cdfa5e5e54102528cbb
musamM921 <135432572+musamM921@users.noreply.github.com>
2023-07-30 22:04:03 +0400

    update19

--- search filter/assets/css/js/script.js
+++ search filter/assets/css/js/script.js
@@ -19,11 +19,36 @@ let listProducts = [
     name: 'Name product white-black-grey',
     price:  300000,
     quantity: 30,
-    image: 'img.jpg2',
+    image: 'img2.jpg',
     nature: {
-        color: []
+        color: ['white', 'black', 'grey'],
+        size: ['S', 'M', 'L'],
+        type: 'Polo'
     }
-
-}
+},
+{
+    id: 3,
+    name: 'Name product black',
+    price:  400000,
+    quantity: 30,
+    image: 'img3.jpg',
+    nature: {
+        color: ['black',],
+        size: ['S', 'M', 'L'],
+        type: 'Polo'
+    }
+},
+{
+    id: 4,
+    name: 'Name product blue-black',
+    price:  400000,
+    quantity: 30,
+    image: 'img4.jpg',
+    nature: {
+        color: ['black', 'blue'],
+        size: ['S', 'M', 'L'],
+        type: 'Polo'
+    }
+},
 ];
 console.log(listProducts);
